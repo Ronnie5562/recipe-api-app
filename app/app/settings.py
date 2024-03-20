3392272efcac8d019f88025fc15b8f5c88a3d09a
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2024-03-20 10:33:04 +0200

    Updates

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,8 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Downloaded Apps
+    'rest_framework',
+    'drf_spectacular',
+
     # My Apps
-    'core'
+    'core',
+    'user'
 ]
 
 MIDDLEWARE = [
@@ -131,3 +136,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
+}
---
+++ app/user/test/test_user_api.py
@@ -0,0 +1,44 @@
+"""
+"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+
+CREATE_USER_URL = reverse('user:create')
+
+def create_user(**params):
+    return get_user_model().objects.create_user(**params)
+
+class PublicUserApiTests(TestCase):
+    """Test the users API (public)"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_valid_user_success(self):
+        """Test creating user with valid payload is successful"""
+        payload = {
+            'email': 'test@example.com'
+        }
+
+    def test_password_too_short_error(self):
+        """Test that the password must be more than 5 characters"""
+        payload = {
+            'email': 'test@example.com',
+            'password': 'test',
+            'name': 'Test User'
+        }
+
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+        user_exists = get_user_model().objects.filter(
+            email=payload['email']
+        ).exists()
+        self.assertFalse(user_exists)
